5108f51b189f3e70dc904be275841591a3d836b0
thorsanchez <ths281@hi.is>
2023-03-29 16:05:47 +0000

    onlinestore og feedback

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -2,7 +2,8 @@ module vidmot.fitness {
     requires javafx.controls;
     requires javafx.fxml;
     //requires javafx.web;
-    //requires javafx.media;
+    requires javafx.media;
+    requires java.desktop;
 
 
     opens vidmot.fitness to javafx.fxml;
--- src/main/java/vidmot/fitness/EnergyWalkController.java
+++ src/main/java/vidmot/fitness/EnergyWalkController.java
@@ -1,8 +1,12 @@
 package vidmot.fitness;
 
-import javafx.event.ActionEvent;
+
 import javafx.fxml.FXML;
 import javafx.scene.image.ImageView;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
+
+import java.io.File;
 
 public class EnergyWalkController {
 
@@ -11,17 +15,22 @@ public class EnergyWalkController {
     @FXML
     private ImageView audioImage1;
 
-    /*
-        public void initialize() {
-            audioImage1.setOnMouseClicked(event -> {
-                String audioFilePath = "path/to/audio/file.mp3";
-                Media audioFile = new Media(new File(audioFilePath).toURI().toString());
-                MediaPlayer mediaPlayer = new MediaPlayer(audioFile);
-                mediaPlayer.play();
-            });
-    */
-    public void fxback(ActionEvent event) {
-        ViewSwitcher.switchTo(View.RUNNING);
+
+    public void initialize() {
+        audioImage1.setOnMouseClicked(event -> {
+            String audioFilePath = "Vika9_Media_src_main_resources_is_media_sample-15s.wav";
+            Media audioFile = new Media(new File(audioFilePath).toURI().toString());
+            MediaPlayer mediaPlayer = new MediaPlayer(audioFile);
+            mediaPlayer.play();
+        });
+
+        /*
+
+        public void fxback (ActionEvent event){
+            ViewSwitcher.switchTo(View.RUNNING);
+        }
+
+         */
     }
 }
 
