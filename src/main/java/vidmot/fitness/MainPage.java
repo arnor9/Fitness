1a6fd8bbc9dadd290aed3871cd7abe02f66b1734
thorsanchez <ths281@hi.is>
2023-04-04 17:38:41 +0000

    recipe takki

--- src/main/java/vidmot/fitness/MainPage.java
+++ src/main/java/vidmot/fitness/MainPage.java
@@ -211,4 +211,34 @@ public class MainPage implements Initializable {
     public void fxlocation(ActionEvent actionEvent) {
         ViewSwitcher.switchTo(View.LACT);
     }
+
+    @FXML
+    private void openWebsite() {
+        String url = "https://www.bonappetit.com/recipes/family-meals/slideshow/big-batch-recipes";
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void openWebsite2() {
+        String url = "https://reciperunner.com";
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void openWebsite3() {
+        String url = "https://www.bbcgoodfood.com/recipes/collection/healthy-recipes-for-weight-loss";
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 }
