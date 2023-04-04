9a7930a1bc627123e973eb3c4cc7d92d93a6a1ca
thorsanchez <ths281@hi.is>
2023-04-04 17:59:45 +0000

    recipe takki update

--- src/main/java/vidmot/fitness/CalendarController.java
+++ src/main/java/vidmot/fitness/CalendarController.java
@@ -105,4 +105,34 @@ public class CalendarController {
     public void fxlocation(ActionEvent actionEvent) {
         ViewSwitcher.switchTo(View.LACT);
     }
+
+    @FXML
+    private void openWebsite() {
+        String url = "https://www.bonappetit.com/recipes/family-meals/slideshow/big-batch-recipes";
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void openWebsite2() {
+        String url = "https://reciperunner.com";
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void openWebsite3() {
+        String url = "https://www.bbcgoodfood.com/recipes/collection/healthy-recipes-for-weight-loss";
+        try {
+            Desktop.getDesktop().browse(new URI(url));
+        } catch (IOException | URISyntaxException e) {
+            e.printStackTrace();
+        }
+    }
 }
